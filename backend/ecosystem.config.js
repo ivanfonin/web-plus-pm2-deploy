7042499d4f8f10acadba2a9df8db660b49ed1fde
Ivan Fonin <ivanfonin@yandex.ru>
2024-09-01 17:05:03 +0300

    refactor: Запускаем приложение там же, где произошла сборка, удаляем лишнее

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -7,7 +7,7 @@ const {
 module.exports = {
   apps: [{
     name: 'backend-app',
-    script: `/home/${DEPLOY_USER}/mesto-backend/app.js`,
+    script: './dist/app.js',
   }],
   deploy: {
     production: {
@@ -17,7 +17,7 @@ module.exports = {
       repo: 'https://github.com/ivanfonin/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && pwd && source /home/${DEPLOY_USER}/.nvm/nvm.sh && npm i && npm run build && mv ./dist/* ${DEPLOY_PATH}/../mesto-backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && source /home/${DEPLOY_USER}/.nvm/nvm.sh && npm i && npm run build`,
     },
   },
 };
