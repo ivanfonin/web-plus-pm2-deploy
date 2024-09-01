61ba4bad0806713f8548c6cb9fb235c2bca06fab
Ivan Fonin <ivanfonin@yandex.ru>
2024-09-01 14:47:31 +0300

    refactor: Исправить pre-deploy

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       ref: DEPLOY_REF, 
       repo: 'https://github.com/ivanfonin/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp -Cr ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp -Cr ./frontend/.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': `cd frontend && npm i && npm run build && mv ./build/* ${DEPLOY_PATH}/mesto-frontend`,
     }
   }
