05b4266b651bc686cbc9cd600fb9e9178983824c
Ivan Fonin <ivanfonin@yandex.ru>
2024-09-01 14:15:03 +0300

    refactor: изменить название репозитория

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF, 
-      repo: 'https://github.com/ivanfonin/nodejs-pm2-deploy.git',
+      repo: 'https://github.com/ivanfonin/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy': `scp -Cr ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': `npm i && npm run build`,
